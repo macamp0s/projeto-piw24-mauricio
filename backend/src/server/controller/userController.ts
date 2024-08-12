0636908626a1b5c9780f2884379aa60dcead3d43
macamp0s <133692429+macamp0s@users.noreply.github.com>
2024-08-12 20:28:01 -0300

    rota delete adicionada

--- backend/src/server/controller/userController.ts
+++ backend/src/server/controller/userController.ts
@@ -11,6 +11,8 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+
+
 export const createUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const newUser: User = req.body;
@@ -22,6 +24,8 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
     res.status(500).json({ message: 'Erro ao criar usuário', error });
   }
 };
+
+
 
 export const updateUser = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -42,5 +46,28 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
     res.status(200).json(users[userIndex]);
   } catch (error) {
     res.status(500).json({ message: 'Error ao atualizar', error });
+  }
+};
+
+
+
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = parseInt(req.params.id, 10);
+
+    let users: User[] = await readUsersFromFile();
+    const userIndex = users.findIndex(user => user.id === userId);
+
+    if (userIndex === -1) {
+      res.status(404).json({ message: 'Erro Usuário não encontrado no banco' });
+      return;
+    }
+
+    users = users.filter(user => user.id !== userId);
+    await writeUsersToFile(users);
+
+    res.status(200).json({ message: 'Usuário deletado' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao deletar', error });
   }
 };
--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUsers, createUser, updateUser } from "../controller/userController";
+import { getUsers, createUser, updateUser, deleteUser } from "../controller/userController";
 
 const router= Router();
 
 router.get('/users', getUsers);
 router.post('/users', createUser);
-router.put('/users/:id', updateUser); 
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export {router};
