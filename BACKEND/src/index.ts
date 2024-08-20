7c44f02e2cccb874111aac6e5df8c1abf4740810
macamp0s <133692429+macamp0s@users.noreply.github.com>
2024-08-20 16:55:31 -0300

    Restarting the project

---
+++ BACKEND/src/index.ts
@@ -0,0 +1,77 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+interface User {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    role: string
+}
+
+const users: User[] = []
+
+app.post('/users', (req, res) => {
+
+    const {
+        name, username, email, password, role } = req.body
+
+    if (!name || !username || !email || !password || !role) {
+        return res.status(400).json({
+            error: {
+                status: 400,
+                name: 'Validation error',
+                message: 'You missed a required field'
+            }
+        })
+    }
+
+    const newUser: User = {
+        id: users.length + 1,
+        name,
+        username,
+        email,
+        password,
+        role
+    }
+
+    users.push(newUser)
+    res.status(201).json({
+        data: newUser
+    })
+})
+
+app.get('/users', (req, res) => {
+    res.json({
+        data: users
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    const user = users.find(u => u.id === parseInt(id))
+
+    if (!user) {
+        return res.status(404).json({
+            error: {
+                status: 404,
+                name: 'Not Found',
+                message: 'User not found'
+            }
+        })
+    }
+    res.json(
+        {
+            data: user
+        })
+})
+
+
+app.listen(port, () => {
+    console.log(`'Servidor escutando a porta ${port} em http://localhost:${port}`)
+})
